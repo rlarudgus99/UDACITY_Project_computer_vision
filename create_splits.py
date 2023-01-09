c7216d67a86c774db4c4fe3639e136e2dba55618
김경현 <98406354+rlarudgus99@users.noreply.github.com>
2023-01-09 15:41:05 +0900

    Add files via upload

---
+++ create_splits.py
@@ -0,0 +1,42 @@
+import argparse
+import glob
+import os
+import random
+
+import numpy as np
+
+import tensorflow as tf
+
+from tensorflow import data
+
+from utils import get_module_logger
+import splitfolders
+
+def split(source, destination):
+    """
+    Create three splits from the processed records. The files should be moved to new folders in the
+    same directory. This folder should be named train, val and test.
+
+    args:
+        - source [str]: source data directory, contains the processed tf records
+        - destination [str]: destination data directory, contains 3 sub folders: train / val / test
+    """
+    
+    
+    # TODO: Implement function
+    splitfolders.ratio(source, output=destination, seed=1234, ratio=(0.8,0.1,0.1))
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
+    parser.add_argument('--source', required=True,
+                        help='source data directory')
+    parser.add_argument('--destination', required=True,
+                        help='destination data directory')
+    args = parser.parse_args()
+
+    logger = get_module_logger(__name__)
+    logger.info('Creating splits...')
+    split(args.source, args.destination)
+    
